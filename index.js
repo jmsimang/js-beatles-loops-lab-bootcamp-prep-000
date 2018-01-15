62df7820ada812091c4a8713fb92d6ae4dbda574
Jabulani Msimango <msimango.jabulani@moov.life>
2018-01-15 15:06:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ function theBeatlesPlay(musicians, instruments) {
   var newArray = {}
   for (let i = 0; i < musicians.length; i++) {
     if (i == 0)
-      newArray.push(musicians[i]+instruments[i])
+      newArray[i] = musicians[i]+instruments[i]
     else
-      newArray.push(musicians[i]+instruments[i])
+      newArray[i] = musicians[i]+instruments[i]
   }
   return newArray
 }
